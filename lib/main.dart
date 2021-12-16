686b904c5b430eba0a37f59cef5e62a9770dd7dc
arshiyanaz1 <shaanarshiya@gmail.com>
2021-12-16 09:27:55 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+import './buttons.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var jokes = [
+    {
+      "question": "What does a baby computer call its father?",
+      "answer": "Data"
+    },
+    {
+      "question": "What's a pencil with two erasers called?",
+      "answer": "Pointless"
+    },
+    {
+      "question": "Why is the calendar always scared?",
+      "answer": "Because it's days are numbered"
+    },
+    {"question": "What do you call a fish with no eye", "answer": "Fsh"},
+  ];
+
+  var jokeIndex = 0;
+
+  changeJokeIndex(String direction) {
+    if (direction == 'next') {
+      if (jokeIndex != jokes.length - 1) {
+        setState(() {
+          jokeIndex++;
+        });
+      } else {
+        jokeIndex = jokeIndex = 0;
+      }
+    } else if (direction == 'prev') {
+      if (jokeIndex != 0) {
+        setState(() {
+          jokeIndex--;
+        });
+      } else {
+        jokeIndex = jokes.length - 1;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Question(question: jokes[jokeIndex]['question'] as String),
+              Answer(answer: jokes[jokeIndex]['answer'] as String),
+              Buttons(changeJokeIndex: changeJokeIndex),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
